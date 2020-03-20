a0b04e1ab3a96c5ed33f7577915dc4121b389e1b
Hamada Sidi Fahari <h.fahari@gmail.com>
2020-03-20 16:55:30 +0100

    [change] use EntityManagerInterface instead EntityManager, add Email Trait

---
+++ src/Model/EmailTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fardus\Traits\Symfony\Model;
+
+/**
+ * trait EmailTrait
+ * @package Fardus\Traits\Symfony\Model
+ */
+trait EmailTrait
+{
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @param string $email
+     * @return static
+     */
+    public function setEmail( ?string $email ) : self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail() : ?string
+    {
+        return $this->email;
+    }
+
+}
