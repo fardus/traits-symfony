cc7cf9f806b5ed7aa2dfc4758ce1a0e0ad2ab4bf
Hamada Sidi Fahari <h.fahari@gmail.com>
2020-04-24 19:03:13 +0200

    [improve] add security trait

---
+++ src/Security/NeedLogin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fardus\Traits\Symfony\Security;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+trait NeedLogin
+{
+    public function login(KernelBrowser $client, UserInterface $user)
+    {
+        $session = $client->getContainer()->get('session');
+        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+        $session->set('_security_main', serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+    }
+
+}
